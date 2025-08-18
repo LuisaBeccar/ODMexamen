505fe6866567484bb83d01c1edc54e5b8b1f7b8a
LuisaBeccar <luisabeccarvarela@gmail.com>
2025-08-18 17:14:33 -0300

    s

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from funciones import limpiar_df, mapear_sexo_por_primer_nombre
 
-urlPDFgh = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/odm.pdf"
-df = limpiar_df(urlPDFgh)
+if __name__ == "__main__":
+    # Lógica principal
+    print("Iniciando análisis")
+   
+    # Llamar funciones
+    
+    #primera obtencion del PDF y limpieza con creacion de variable ODM sin puntos Nacionales
+    urlPDFgh = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/odm.pdf"
+    df = limpiar_df(urlPDFgh)
 
-url = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/ns_def.csv"
-df = mapear_sexo_por_primer_nombre(df, url, nombre_col_original='NOMBRE', sexo_col='SEXO')
+    # creacion de variable sexo
+    url = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/ns_def.csv"
+    df = mapear_sexo_por_primer_nombre(df, url, nombre_col_original='NOMBRE', sexo_col='SEXO')
 
-
-df
+    print(df)
