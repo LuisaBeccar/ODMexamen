e0495b4b6020c93e1499a1509313b78cdf32c992
LuisaBeccar <luisabeccarvarela@gmail.com>
2025-08-19 10:22:45 -0300

    .

--- generar_data/main.py
+++ generar_data/main.py
@@ -1,4 +1,4 @@
-from funciones import limpiar_df, mapear_sexo_por_primer_nombre
+from funciones import limpiar_df, mapear_sexo_por_primer_nombre, mapear_universidades
 
 if __name__ == "__main__":
     # Lógica principal
@@ -14,7 +14,11 @@ if __name__ == "__main__":
     url = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/ns_def.csv"
     df = mapear_sexo_por_primer_nombre(df, url, nombre_col_original='NOMBRE', sexo_col='SEXO')
 
+    url = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/universidades.csv"
+    df = mapear_universidades(df, url, nombre_col_original='UNIVERSIDAD')
+
+
     print(df)
-    df.to_csv("odm_limpio.csv", index=False)
-    print("Análisis finalizado. Archivo 'odm_limpio.csv' creado.")
+    #df.to_csv("odm_limpio.csv", index=False)
+    #print("Análisis finalizado. Archivo 'odm_limpio.csv' creado.")
 
