760caf09797bc8bfb801996b52b1a7c10c4f39c1
Luisa Beccar Varela <159951478+LuisaBeccar@users.noreply.github.com>
2025-08-21 08:43:18 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -3,7 +3,79 @@ import pandas as pd
 import pdfplumber
 import requests
 
+def limpiar_ODM2025(url_pdf: str, nombre_archivo: str = "ODM2025.pdf") -> pd.DataFrame:
+    """
+    Descarga, extrae y limpia datos de un PDF en un DataFrame listo para análisis.
+    Args:
+        url_pdf (str): URL del archivo PDF a descargar.
+        nombre_archivo (str): Nombre con que se guardará el PDF localmente.
+    Returns:
+        pd.DataFrame: DataFrame limpio y transformado.
+    """
 
+    # Descargar PDF si no existe localmente
+    if not os.path.isfile(nombre_archivo):
+        r = requests.get(url_pdf)
+        with open(nombre_archivo, "wb") as f:
+            f.write(r.content)
+
+
+    # Extraer tablas del PDF
+    data = []
+    with pdfplumber.open(nombre_archivo) as pdf:
+        for page in pdf.pages:
+            table = page.extract_table()
+            if table:
+                data.extend(table)
+
+    # -----------------
+    # Crear DataFrame y limpiar encabezados
+    df = pd.DataFrame(data[1:], columns=data[1])
+    df.columns = df.columns.str.replace("\n", " ", regex=True)
+ 
+    # Renombrar columnas
+    rename_dict = {
+        "Institución formadora": "UNIVERSIDAD",
+        "Promedio": "PROMEDIO_CARRERA",
+        "Fecha de Expedición de Título": "FECHA_TITULO",
+        "Especialidad": "ESPECIALIDAD",
+        "Puntaje obtenido en el examen": "NOTA_EXAMEN",
+        "Tipo Uni": "TIPO_UNI",
+        "Componente": "COMPONENTE",
+        "Puntaje Final": "PUNTAJE"
+    }
+    df = df.rename(columns=rename_dict)
+
+    # Eliminar filas no deseadas
+    df = df[~df.isin(["DNI"]).any(axis=1)] # filas donde se repite el encabezado
+    df = df[df['DNI'].str.strip().astype(bool)]  # elimina filas vacías (con la celda de apellido vacio)
+    df = df.dropna().reset_index(drop=True) # reindexar
+
+    
+    # Ajustes de formato
+    df["FECHA_TITULO"] = pd.to_datetime(df["FECHA_TITULO"], format="%d-%m-%Y", errors="coerce")
+    # Floats
+    cols_f = ["PROMEDIO_CARRERA", "PUNTAJE"]
+    df[cols_f] = df[cols_f].replace(",", ".", regex=True).replace("", float('nan')).astype(float)
+    # Enteros
+    cols_i = ['NOTA_EXAMEN', 'COMPONENTE', 'ODM']
+    for col in cols_i:
+        df[col] = pd.to_numeric(df[col], errors="coerce").astype("Int64")
+    df['PUNTAJE_CRUDO'] = df['PUNTAJE']-df['COMPONENTE']
+    df["COMPONENTE"] = df["COMPONENTE"].replace("", 0) # que en vez de vacio diga cero
+    
+    # Reemplazos globales en todas las celdas
+    df = df.replace({r'\n': ' ', "en tramite": "01-07-2025"}, regex=True) #crear el espacio entre los nombres en vez de "\n" y poner la fecha que elegi en vez de "en tramite"
+    # Tiempo entre recibido y el examen (1 julio 2025)
+    # Definir la fecha cero
+    fecha_cero = pd.to_datetime("2025-07-01")
+
+    # Asegurar que FECHA_TITULO es datetime
+    df["FECHA_TITULO"] = pd.to_datetime(df["FECHA_TITULO"], format="%d-%m-%Y", errors="coerce")
+    # Calcular diferencia en días
+    df["DIAS_DESDE_TITULO"] = (fecha_cero - df["FECHA_TITULO"]).dt.days
+
+    return df
 
 def limpiar_df(url_pdf: str, nombre_archivo: str = "odm.pdf") -> pd.DataFrame:
     """
@@ -34,16 +106,7 @@ def limpiar_df(url_pdf: str, nombre_archivo: str = "odm.pdf") -> pd.DataFrame:
     # Crear DataFrame y limpiar encabezados
     df = pd.DataFrame(data[1:], columns=data[1])
     df.columns = df.columns.str.replace("\n", " ", regex=True)
-
-     # Reemplazos globales en todas las celdas
-    df = df.replace({r'\n': ' ', "en tramite": "01-07-2025"}, regex=True) #crear el espacio entre los nombres en vez de "\n" y poner la fecha que elegi en vez de "en tramite"
-    df["COMPONENTE"] = df["COMPONENTE"].replace("", 0) # que en vez de vacio diga cero
-
-    # Eliminar filas no deseadas
-    df = df[~df.isin(["DNI"]).any(axis=1)] # filas donde se repite el encabezado
-    df = df[df['APELLIDO'].str.strip().astype(bool)]  # elimina filas vacías (con la celda de apellido vacio)
-    df = df.dropna().reset_index(drop=True) # reindexar
-    
+ 
     # Renombrar columnas
     rename_dict = {
         "Número de documento": "DNI",
@@ -59,22 +122,29 @@ def limpiar_df(url_pdf: str, nombre_archivo: str = "odm.pdf") -> pd.DataFrame:
     }
     df = df.rename(columns=rename_dict)
 
-   
+    # Eliminar filas no deseadas
+    df = df[~df.isin(["DNI"]).any(axis=1)] # filas donde se repite el encabezado
+    df = df[df['APELLIDO'].str.strip().astype(bool)]  # elimina filas vacías (con la celda de apellido vacio)
+    df = df.dropna().reset_index(drop=True) # reindexar
 
+    
     # Ajustes de formato
     df["NOMBRE"] = df["NOMBRE"].str.upper()
     df["APELLIDO"] = df["APELLIDO"].str.upper()
     df["FECHA_TITULO"] = pd.to_datetime(df["FECHA_TITULO"], format="%d-%m-%Y", errors="coerce")
-
+    # Floats
     cols_f = ["PROMEDIO_CARRERA", "PUNTAJE"]
     df[cols_f] = df[cols_f].replace(",", ".", regex=True).replace("", float('nan')).astype(float)
-    
+    # Enteros
     cols_i = ['NOTA_EXAMEN', 'COMPONENTE', 'ODM']
     for col in cols_i:
         df[col] = pd.to_numeric(df[col], errors="coerce").astype("Int64")
     df['PUNTAJE_CRUDO'] = df['PUNTAJE']-df['COMPONENTE']
-
-    # Tiempo entre recibido y inscripcion aprox al examen (1 abril 2025)
+    df["COMPONENTE"] = df["COMPONENTE"].replace("", 0) # que en vez de vacio diga cero
+    
+    # Reemplazos globales en todas las celdas
+    df = df.replace({r'\n': ' ', "en tramite": "01-07-2025"}, regex=True) #crear el espacio entre los nombres en vez de "\n" y poner la fecha que elegi en vez de "en tramite"
+    # Tiempo entre recibido y el examen (1 julio 2025)
     # Definir la fecha cero
     fecha_cero = pd.to_datetime("2025-07-01")
 
