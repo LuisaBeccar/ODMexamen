b5690e7cde6e26d1907feb80773e50cec604e393
Luisa Beccar Varela <159951478+LuisaBeccar@users.noreply.github.com>
2025-08-21 09:08:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-from funciones import limpiar_df, mapear_sexo_por_primer_nombre, asignar_origen, asignar_ranking, mapear_universidades, limpiar_ODM2025
+from funciones import limpiar_df, limpiar_df2, mapear_sexo_por_primer_nombre, asignar_origen, asignar_ranking, mapear_universidades, limpiar_ODM2025
+from funciones2 import  obtener_odm, obtener_ODM2025
 
 if __name__ == "__main__":
     # Lógica principal
     print("Iniciando análisis")
    
     # Llamar funciones
-    
+    """
     #primera obtencion del PDF y limpieza con creacion de variable ODM sin puntos Nacionales
     urlPDFgh = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/generar_data/odm.pdf"
     df = limpiar_df(urlPDFgh)
 
     urlPDFODM2025gh = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/generar_data/ODM2025.pdf"
     df = limpiar_df(urlPDFODM2025gh)
+    """
+    
+    urlPDFgh = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/generar_data/odm.pdf"
+    dfodm = obtener_odm(urlPDFgh)
 
+    urlPDFODM2025gh = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/generar_data/ODM2025.pdf"
+    dfODM2025 = obtener_ODM2025(urlPDFODM2025gh)
+    
+    df = mergeODFS(dfodm, dfODM2025)
+    df = limpiar_df(df)
+
+    
     # creacion de variable sexo
     url = "https://raw.githubusercontent.com/LuisaBeccar/ODMexamen/main/generar_data/ns_def.csv"
     #df.columns = df.columns.str.strip().str.upper()
